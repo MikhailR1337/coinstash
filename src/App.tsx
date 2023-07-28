e574efe2386ff287af63ef496bff150aafbeac94
Mikhail Rogov <m.rogov@mel.fm>
2023-07-28 08:51:46 +0800

    added react ts tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="flex justify-center flex-wrap items-center h-screen">
+      Hello app
+    </div>
+  );
+}
+
+export default App;
